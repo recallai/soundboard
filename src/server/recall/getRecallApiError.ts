01446bdbca070c2046d109ad635caf2b2f2b62e1
Gerry Saporito <gerrysaporito@gmail.com>
2025-07-09 16:39:27 -0700

    error

--- src/server/recall/getRecallApiError.ts
+++ src/server/recall/getRecallApiError.ts
@@ -1,8 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getRecallApiError = (data: any): string => {
-    if (data?.detail) {
+export const getRecallApiError = (data: unknown): string => {
+    if (typeof data !== 'object' || data === null) {
+        return JSON.stringify(data);
+    }
+
+    if ('detail' in data) {
         return getSanitizedErrorMessage(data);
-    } else if (data?.non_field_errors) {
+    } else if ('non_field_errors' in data) {
         return getSanitizedErrorMessage(data?.non_field_errors);
     }
     if (typeof data === 'string') {
@@ -12,7 +15,7 @@ export const getRecallApiError = (data: any): string => {
     }
 }
 
-const getSanitizedErrorMessage = (data: any): string => {
+const getSanitizedErrorMessage = (data: unknown): string => {
     if (Array.isArray(data)) {
         return data?.join(". ");
     } else if (typeof data === 'string') {
